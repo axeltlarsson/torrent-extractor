ed460d2acab13d0dd298613de88876bbc66037a1
AxelTLarsson <mail@axellarsson.nu>
2014-11-19 22:08:42 +0100

    Fixed small typo

--- torrent_extracter_runner.py
+++ torrent_extracter_runner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""""Convenience wrapper for running torrent_extracter directly from source treee"""
+""""Convenience wrapper for running torrent_extracter directly from source tree"""
 
 from torrent_extracter.torrent_extracter import main
 
