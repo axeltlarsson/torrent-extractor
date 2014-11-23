6c59cb3de07973795d103e68b41a5b1474286c7f
560edf300c324bae774037c835e4d263fe81bfa2 <mail@axellarsson.nu>
2014-11-23 19:55:52 +0100

    Some write permission checks, fixed a logging bug

--- torrent_extracter/torrent_extracter.py
+++ torrent_extracter/torrent_extracter.py
@@ -19,26 +19,27 @@ def main():
 	#	Deal with args and settings
 	#-----------------------------------
 	parser = argparse.ArgumentParser(description="Intelligently copies/extracts films and tv series to respective target directories")
-	parser.add_argument("-t", "--tv_path", help="path to destination folder for TV shows")
-	parser.add_argument("-f", "--film_path", help="path to destination folder for films")
+	parser.add_argument("-t", "--tv_path", help="path to destination folder for TV shows, default is /tmp/films")
+	parser.add_argument("-f", "--film_path", help="path to destination folder for films, default is /tmp/tv_series")
 	parser.add_argument("-d", "--debug", action="store_true", help="set this flag to prevent log files from being written and enable DEBUG-level output to console")
+	parser.add_argument("-l", "--log_file", help="path to log file, default is ./torrent_extracter.log")
 	parser.add_argument("torrent", help="the file path to the torrent to extract")
 	args = parser.parse_args()
 
 	settings = Settings()
 	settings.tv_path = os.path.normpath(args.tv_path or "/tmp/tv_series")
-	settings.film_path = os.path.normpath(args.film_path or "/tmp/films")	
-	
+	settings.film_path = os.path.normpath(args.film_path or "/tmp/films")
+	log_file = os.path.normpath(args.log_file or "./torrent_extracter.log")
+
 	#-----------------------------------
 	#	Set up logging
 	#-----------------------------------
 	log = logging.getLogger("t_e")
-	log_level = logging.DEBUG if args.debug else logging.INFO
-	log.setLevel(log_level)
+	log.setLevel(logging.DEBUG) # let the handlers filter instead
 
 	# Set up a console handler
 	console_handler = StreamHandler(stream=sys.stderr)
-	console_handler.setLevel(log_level)
+	console_handler.setLevel(logging.DEBUG if args.debug else logging.INFO)
 	console_formatter = logging.Formatter(fmt='[%(levelname)s] %(message)s', 
 		datefmt='%b %d %H:%M:%S')
 	console_handler.setFormatter(console_formatter)
@@ -46,7 +47,7 @@ def main():
 
 	# Set up a file handler that also rotates the logs, unless of course debug mode is set, then we only log to console
 	if not args.debug:
-		file_handler = RotatingFileHandler('torrent_extracter.log', 
+		file_handler = RotatingFileHandler(log_file, 
 			maxBytes=30000, # gives about 220 lines before rotating
 			backupCount=5, # keep 5 backups
 			delay=True) # do not write the log file until first log message
@@ -55,9 +56,18 @@ def main():
 			datefmt='%b %d %H:%M:%S')
 		file_handler.setFormatter(file_formatter)
 		log.addHandler(file_handler)
-			
+	
+	# Some extra checks on the arguments		
 	if not os.path.exists(os.path.normpath(args.torrent)):
 		log.critical(os.path.normpath(args.torrent) + " does not exist, exiting.")
+		sys.exit(1)
+	if not os.access(os.path.dirname(os.path.abspath(log_file)), os.W_OK):
+		log.warning("No write permission to " + log_file + ", no log file will be written.")
+	if not os.access(os.path.dirname(os.path.abspath(settings.tv_path)), os.W_OK):
+		log.critical("No write permission to " + os.path.abspath(settings.tv_path) + ", exiting.")
+		sys.exit(1)
+	if not os.access(os.path.dirname(os.path.abspath(settings.film_path)), os.W_OK):
+		log.critical("No write permission to " + os.path.abspath(settings.film_path) + ", exiting.")
 		sys.exit(1)
 
 	#-----------------------------------
